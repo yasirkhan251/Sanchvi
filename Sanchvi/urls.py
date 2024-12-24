38f147327691c0f0602b1114f21fd49b599dc5e0
Sanchvi <you@example.com>
2024-12-24 10:05:19 +0000

    Sanchvi V12.24.7 Added Regular as option and also updated gowns

--- Sanchvi/urls.py
+++ Sanchvi/urls.py
@@ -31,4 +31,8 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
